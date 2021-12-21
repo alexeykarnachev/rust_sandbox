263d13625e770359173f21284c7be9897835b716
Alexey Karnachev <alekseykarnachev@gmail.com>
2021-12-21 20:03:38 +0300

    add prefix tree

--- src/trees/prefix_tree.rs
+++ src/trees/prefix_tree.rs
@@ -5,23 +5,46 @@ type NodesMap = HashMap<char, Box<Node>>;
 #[derive(Debug)]
 pub struct Node {
     children: NodesMap,
+    value: Option<String>,
 }
 
 impl Node {
     pub fn new() -> Self {
         Self {
             children: HashMap::new(),
+            value: None,
         }
     }
 
-    pub fn add(&mut self, mut chars: Chars) {
+    pub fn add(&mut self, mut chars: Chars, value: String) {
         if let Some(child_char) = chars.next() {
             if !self.children.contains_key(&child_char) {
                 self.children.insert(child_char, Box::new(Node::new()));
             }
             let node = self.children.get_mut(&child_char).unwrap();
-            node.add(chars);
+            node.add(chars, value);
+        } else {
+            self.value = Some(value);
         }
+    }
+
+    pub fn get(&self, mut prefix: Chars) -> Vec<String> {
+        let mut result = Vec::new();
+        if let Some(child_char) = prefix.next() {
+            if let Some(child_node) = self.children.get(&child_char) {
+                let mut child_result = child_node.get(prefix);
+                result.append(&mut child_result);
+            }
+        } else {
+            if let Some(value) = &self.value {
+                result.push(value.clone());
+            }
+            for (_, child_node) in &self.children {
+                let mut child_result = child_node.get(prefix.clone());
+                result.append(&mut child_result);
+            }
+        }
+        result
     }
 }
 
@@ -37,16 +60,31 @@ impl Trie {
         }
     }
 
-    pub fn add(&mut self, str: String) {
-        let mut chars = str.chars();
+    pub fn add(&mut self, value: String) {
+        let chars_value = value.clone();
+        let mut chars = chars_value.chars();
         if let Some(root_char) = chars.next() {
             if !self.roots.contains_key(&root_char) {
                 self.roots.insert(root_char, Box::new(Node::new()));
             }
             let node = self.roots.get_mut(&root_char).unwrap();
-            node.add(chars);
+            node.add(chars, value);
         }
-        println!("{:#?}", self);
+    }
+
+    pub fn get(&self, prefix: String) -> Vec<String> {
+        let mut result = Vec::new();
+        let mut chars = prefix.chars();
+        if let Some(root_char) = chars.next() {
+            if let Some(root_node) = self.roots.get(&root_char) {
+                let mut child_result = root_node.get(chars.clone());
+                result.append(&mut child_result);
+                if let (Some(value), None) = (&root_node.value, chars.next()) {
+                    result.push(value.clone());
+                }
+            }
+        }
+        result
     }
 }
 
@@ -56,9 +94,16 @@ mod tests {
     #[test]
     fn it_works() {
         let mut trie = Trie::new();
-        trie.add("hello".to_string());
-        trie.add("hells".to_string());
-        trie.add("hello".to_string());
-        trie.add("hellow".to_string());
+        trie.add("aaa".to_string());
+        trie.add("aab".to_string());
+        trie.add("aac".to_string());
+        trie.add("aba".to_string());
+        trie.add("abb".to_string());
+        trie.add("aa".to_string());
+        trie.add("ab".to_string());
+        trie.add("a".to_string());
+
+        let result = trie.get("".to_string());
+        println!("{:#?}", result);
     }
 }
